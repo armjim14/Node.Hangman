18ae243d908d222d7fe3b2f60623323e7bef49f6
armjim14 <ajimenez1497@gmail.com>
2019-07-06 22:54:11 -0700

    chances variable is off

--- game.js
+++ game.js
@@ -4,16 +4,20 @@ var list = ["pen", "paper", "desk", "marker", "eraser", "ruler", "pencil", "back
 
 var word = list[Math.floor(Math.random() * list.length)]
 var chances = 7;
-
+var on = true;
 var guess = [];
+var history = [];
 
 for ( let i = 0; i < word.length; i++ ){
     guess.push("-");
 }
 
+console.log("");
 console.log(guess.join(" "));
+console.log("");
 
 function game(){
+if (on){
     inq.prompt([
         {
             type: "input", 
@@ -26,10 +30,11 @@ function game(){
             console.log("Please select a single letter")
             console.log("");
             game();
-        } else {
+        } else if ( !history.includes(res.user.toLowerCase()) ) {
 
             var wrong = true;
             var letter = res.user.toLowerCase();
+            history.push(letter);
 
             for ( let i = 0; i < word.length; i++ ){
                 if ( letter == word[i] ){
@@ -37,28 +42,42 @@ function game(){
                     wrong = false;
                 }
             }
+            console.log("");
             console.log(guess.join(" "));
             console.log(`Your chances remaining ${chances}`)
+            console.log("");
             if (wrong){
                 chances--;
             }
 
             if ( chances == 0 ){
                 youlose();
-            } else if ( word == guess.join(" ") ){
+            } else if ( !guess.includes("-") ){
                 youwon();
             }
             game();
+        } else {
+            console.log("");
+            console.log("That letter was already selected");
+            console.log("");
+            game();
         }
     })
+}
 }
 
 game()
 
 function youwon(){
-
+    on = false;
+    console.log("");
+    console.log("You Won the word was " + word);
+    console.log("");
 }
 
 function youlose(){
-    
+    on = false;
+    console.log("");
+    console.log("You Lost the word was " + word);
+    console.log("");
 }
